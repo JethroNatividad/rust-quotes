5388c97ace42dad8ea3004c006dbb51ca21bd721
JethroNatividad <natividad.jet@gmail.com>
2023-12-10 17:36:44 +0800

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::Write;
+// Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
+
+// verbs: prompts, display
+// nouns: quote, author
+
+// Input: quote and author
+// Process: Build the output
+// Output: the output
+
+// Test
+// Input: What is the quote?: These aren't the droids you're looking for., Who said it?: Obi-Wan Kenobi
+// Output: Obi-Wan Kenobi says, "These aren't the droids you're looking for."
+
+
+
+fn main() -> io::Result<()> {
+    let mut quote = String::new();
+    let mut author = String::new();
+    print!("What is the quote?: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut quote)?;
+    print!("Who said it?: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut author)?;
+    
+    println!("{}", author.trim().to_owned() + " says, " + "\"" + quote.trim() + "\"");
+    Ok(())
 }
